2b806cc17a3b50837980769961687a50d346c5b7
jose clavo tafur <jclavotafur@gmail.com>
2020-05-11 15:52:48 -0300

    add barcode to app

--- sampleBarcodeLector/src/app/home/home.page.ts
+++ sampleBarcodeLector/src/app/home/home.page.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  constructor() {}
+  public result: string = '';
+  public error: string = '';
 
+  constructor(private barcodeScanner: BarcodeScanner) { }
+
+  openLectorQR() {
+
+    this.barcodeScanner.scan().then(barcodeData => {
+      this.result = barcodeData.text;
+      console.log('Barcode data', this.result);
+    }).catch(err => {
+      this.error = err;
+      console.log('Error', this.error);
+    });
+
+  }
 }
